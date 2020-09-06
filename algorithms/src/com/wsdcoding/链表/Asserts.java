9524d9c24ecf601675039e79438d6b79ee69d291
wsdcoding <weishide1@gmail.com>
2020-09-07 03:12:24 +0800

    organize notes

---
+++ algorithms/src/com/wsdcoding/链表/Asserts.java
@@ -0,0 +1,16 @@
+package com.wsdcoding.链表;
+
+/**
+ * @Description:
+ * @Authror Mohanjun
+ * @Date 2020/5/16 18:40
+ */
+public class Asserts {
+    public static void test(boolean value) {
+        try {
+            if (!value) throw new Exception("测试未通过");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ algorithms/src/com/wsdcoding/链表/circle/SingleCircleLinkedList.java
@@ -0,0 +1,9 @@
+package com.wsdcoding.链表.circle;
+
+/**
+ * @Description:
+ * @Authror Mohanjun
+ * @Date 2020/5/15 15:09
+ */
+public class SingleCircleLinkedList {
+}
