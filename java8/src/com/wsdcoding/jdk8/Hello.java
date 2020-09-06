af62aa6647915f308b1a0d7bb46fb6b05a9589b8
wsdcoding <weishide1@gmail.com>
2020-09-07 02:31:33 +0800

    organize notes

---
+++ java8/src/com/wsdcoding/jdk8/Hello.java
@@ -0,0 +1,11 @@
+package com.wsdcoding.jdk8;
+
+/**
+ * @Description:
+ * @Authror wsdcoding
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello jdk1.8");
+    }
+}
